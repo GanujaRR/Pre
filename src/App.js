e64a5be0cc4dfb886552e59683f931c7815e6453
Robert Rzedzicki <34694@student.pwsztar.edu.pl>
2022-05-13 17:12:51 +0200

    Laboratorium 10

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from "./components/navbar";
+import { Outlet } from "react-router-dom";
+
+function App() {
+  return (
+      <div className="container-fluid">
+          <Navbar />
+          <div className="container">
+              <Outlet />
+          </div>
+      </div>
+  );
+}
+
+export default App;
